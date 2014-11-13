a925029651cd55ac3e5f2a0e4b1067bf00d02912
dahuan <liuyujiahuan@163.com>
2014-11-13 16:32:39 +0800

    Initial Commit

---
+++ CoreData Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData Demo
+//
+//  Created by dahuan on 14/11/13.
+//  Copyright (c) 2014年 DongShenNianYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreData Demo
+//
+//  Created by dahuan on 14/11/13.
+//  Copyright (c) 2014年 DongShenNianYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
